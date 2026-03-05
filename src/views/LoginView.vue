325bd726f05599932732d453f6c50c89d6bc5654
Philemon5MUVUNYI <muvunyiphilemon22@gmail.com>
2026-03-05 11:40:10 +0200

    testing the navigation of the pages and structure

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Bus Ticket System Login</h1>
+  </div>
+  <div>
+    <h1>Login Page</h1>
+
+    <router-link to="/user-dashboard">User Dashboard</router-link>
+
+    <br>
+
+    <router-link to="/admin-dashboard">Admin Dashboard</router-link>
+
+  </div>
+</template>
