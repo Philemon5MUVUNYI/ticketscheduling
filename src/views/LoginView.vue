fdd25883cb6c1181d7f65400192bb615ca8e7a0b
Philemon5MUVUNYI <muvunyiphilemon22@gmail.com>
2026-03-05 14:12:17 +0200

    Adding the LandingView.vue page and

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,51 @@
+<script setup>
+import { ref } from "vue"
+import { useRouter } from "vue-router"
+import { useAuthStore } from "../stores/authStore"
+
+const email = ref("")
+const password = ref("")
+
+const router = useRouter()
+const auth = useAuthStore()
+
+function login() {
+
+  if (email.value === "admin@gmail.com" && password.value === "admin") {
+    auth.login("Admin", "admin")
+    router.push("/admin-dashboard")
+  }
+
+  else if (email.value === "user@gmail.com" && password.value === "user") {
+    auth.login("User", "user")
+    router.push("/user-dashboard")
+  }
+
+  else {
+    alert("Invalid credentials")
+  }
+
+}
+</script>
+
 <template>
-  <div>
-    <h1>Bus Ticket System Login</h1>
-  </div>
-  <div>
-    <h1>Login Page</h1>
 
-    <router-link to="/user-dashboard">User Dashboard</router-link>
+  <h1>Bus Ticket System Login</h1>
 
-    <br>
+  <form @submit.prevent="login">
 
-    <router-link to="/admin-dashboard">Admin Dashboard</router-link>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="email" required>
+    </div>
 
-  </div>
+    <div>
+      <label>Password</label>
+      <input type="password" v-model="password" required>
+    </div>
+
+    <button type="submit">Login</button>
+
+  </form>
+
 </template>
